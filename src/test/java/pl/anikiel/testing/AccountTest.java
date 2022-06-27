ce5cb5c6c078406941e909f67eba54e2ec116eec
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-06-27 17:55:23 +0200

    given-when-then

--- src/test/java/pl/anikiel/testing/AccountTest.java
+++ src/test/java/pl/anikiel/testing/AccountTest.java
@@ -7,9 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AccountTest {
 
     @Test
-    public void myTest() {
+    public void newAccountShouldNotBeActiveAfterCreation() {
+        //given
         Account newAccount = new Account();
 
+        //then
         assertFalse(newAccount.isActive(), "Check if new account is not active");
+    }
+
+    @Test
+    public void accountShouldBeActiveAfterCreation() {
+        //given
+        Account newAccount = new Account();
+
+        //when
+        newAccount.activate();
+
+        //then
+        assertTrue(newAccount.isActive(), "'active' is set to true on activated account");
     }
 }
