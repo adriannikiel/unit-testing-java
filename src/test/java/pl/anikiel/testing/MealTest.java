ee9f4d3d90a7459634f5df764e5137be4fe81ae4
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-10-10 23:15:24 +0200

    Dynamic tests

--- src/test/java/pl/anikiel/testing/MealTest.java
+++ src/test/java/pl/anikiel/testing/MealTest.java
@@ -1,17 +1,23 @@
 package pl.anikiel.testing;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class MealTest {
 
@@ -116,5 +122,42 @@ class MealTest {
             throw new IllegalArgumentException();
         }
         assertThat(price).isLessThan(20);
+    }
+
+    @TestFactory
+    Collection<DynamicTest> dynamicTestCollection() {
+        return List.of(
+                dynamicTest("Dynamic test 1", () -> assertThat(5).isLessThan(6)),
+                dynamicTest("Dynamic test 2", () -> assertEquals(4, 2 * 2))
+        );
+    }
+
+    @TestFactory
+    Collection<DynamicTest> calculateMealPrices() {
+        Order order = new Order();
+        order.addMealToOrder(new Meal(10, 2, "Hamburger"));
+        order.addMealToOrder(new Meal(7, 4, "Fries"));
+        order.addMealToOrder(new Meal(22, 3, "Pizza"));
+
+        Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for (int i = 0; i < order.getMeals().size(); i++) {
+            int price = order.getMeals().get(i).getPrice();
+            int quantity = order.getMeals().get(i).getQuantity();
+
+            Executable executable = () -> {
+                assertThat(calculatePrice(price, quantity)).isLessThan(67);
+            };
+
+            String name = "Test name: " + i;
+            DynamicTest dynamicTest = DynamicTest.dynamicTest(name, executable);
+            dynamicTests.add(dynamicTest);
+        }
+        
+        return dynamicTests;
+    }
+
+    private int calculatePrice(int price, int quantity) {
+        return price * quantity;
     }
 }
