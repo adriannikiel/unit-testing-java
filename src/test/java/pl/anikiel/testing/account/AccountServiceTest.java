f8564a5f2cc832815fcc87c92320f1e08a9431ca
Adrian Nikiel <adrian.nikiel@gmail.com>
2022-10-27 22:46:11 +0200

    mocks definition and usage

--- src/test/java/pl/anikiel/testing/account/AccountServiceTest.java
+++ src/test/java/pl/anikiel/testing/account/AccountServiceTest.java
@@ -2,22 +2,55 @@ package pl.anikiel.testing.account;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-class AccountServiceTest {
-
+public class AccountServiceTest {
     @Test
     void getAllActiveAccounts() {
         //given
-        AccountRepository accountRepository = new AccountRepositoryStub();
+        List<Account> accounts = prepareAccountData();
+        AccountRepository accountRepository = mock(AccountRepository.class);
         AccountService accountService = new AccountService(accountRepository);
+        //when(accountRepository.getAllAccounts()).thenReturn(accounts);
+        given(accountRepository.getAllAccounts()).willReturn(accounts);
 
         //when
         List<Account> accountList = accountService.getAllActiveAccounts();
 
         //then
         assertThat(accountList).hasSize(2);
+    }
+
+    @Test
+    void getNoActiveAccounts() {
+        //given
+        List<Account> accounts = prepareAccountData();
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+        given(accountRepository.getAllAccounts()).willReturn(List.of());
+
+        //when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        //then
+        assertThat(accountList).hasSize(0);
+    }
+
+    private List<Account> prepareAccountData() {
+        Address address1 = new Address("Kwiatowa", "33/5");
+        Account account1 = new Account(address1);
+
+        Account account2 = new Account();
+
+        Address address3 = new Address("Piekarska", "12b");
+        Account account3 = new Account(address3);
+
+        return List.of(account1, account2, account3);
     }
 }
